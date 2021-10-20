1b283e7470dd0eb61debf83422b97ad8b76069d0
Nuntanat Rongkapan <seenrongkapan@Nuntanats-MacBook-Pro-2.local>
2021-10-20 14:21:45 -0700

    list page edited

--- rongkapan.seen/js/app.js
+++ rongkapan.seen/js/app.js
@@ -7,10 +7,16 @@ $(()=> {
 	//EVENT DELIGATION
 	$(document)
 
+
+	//FORM SUBMIT
 	.on("submit","#signin-form",function(e) {
 		e.preventDefault();
 		checkSigninForm();
 	})
+	.on("submit","#list-add-form",function(e) {
+      e.preventDefault();
+   	})
+
 
 	//ANCHOR CLICKS
 	.on("click",".js-logout",function(e) {
@@ -21,20 +27,22 @@ $(()=> {
 
 
 	.on("click","[data-activate]",function(e){
-		e.preventDefault();
 		let target = $(this).data("activate");
 		$(target).addClass("active")
 	})
 	.on("click","[data-deactivate]",function(e){
-		e.preventDefault();
 		let target = $(this).data("deactivate");
 		$(target).removeClass("active")
 	})
 	.on("click","[data-toggle]",function(e){
-		e.preventDefault();
 		let target = $(this).data("toggle");
 		$(target).toggleClass("active")
 	})
+	 .on("click","[data-activateone]",function(e){
+      let target = $(this).data("activateone");
+      console.log(target)
+      $(target).addClass("active").siblings().removeClass('active');
+   	})
 
 	;
 
@@ -43,5 +51,10 @@ $(()=> {
       $(this).html($(target).html());
    	})
 
+	$({
+	"#page-recent":".nav-icon-set li:nth-child(1)",
+      "#page-list":".nav-icon-set li:nth-child(2)",
+      "#page-user-profile":".nav-icon-set li:nth-child(3)",
+	}[location.hash]).addClass("active");
 
 });
